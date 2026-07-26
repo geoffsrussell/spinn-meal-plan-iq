948922837b68d1eb5f5149d8acb30e8ed4b12dc1
geoffsrussell <grussell@spinnus.com>
2026-07-25 22:25:24 -0700

    updated

--- netlify/functions/auth-logout.js
+++
@@ -1,4 +0,0 @@
-import { json, cookie } from './_shared/http.js';
-export async function handler() {
-  return json(200, { connected: false }, { 'set-cookie': cookie('kroger_session', '', { maxAge: 0 }) });
-}
--- scripts/verify-build.mjs
+++
@@ -1,3 +0,0 @@
-import { access } from 'node:fs/promises';
-await access('public/index.html');
-console.log('Static app and Netlify Functions are ready.');
